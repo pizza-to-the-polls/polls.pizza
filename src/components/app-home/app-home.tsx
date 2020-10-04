2eb80a7e80fa4d1bc349ac29b215cb0de9d88309
Noah Manger <noah.manger@gmail.com>
2020-10-04 16:57:18 -0700

    Cards on home page

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -7,11 +7,13 @@ import { Component, h } from "@stencil/core";
 export class AppHome {
   public render() {
     return (
-      <section class="hero">
-        <div class="container">
+      <div>
+        <section class="hero">
           <div class="intro">
-            <p>Pizza to the Polls is making democracy delicious by delivering free food for all to polling places with long lines.</p>
-            <p>Send us reports of long lines and we'll send in the delicious reinforcements.</p>
+            <div class="container">
+              <p>Pizza to the Polls is making democracy delicious by delivering free food for all to polling places with long lines.</p>
+              <p>Send us reports of long lines and we'll send in the delicious reinforcements.</p>
+            </div>
           </div>
           <section class="dashboard">
             <div class="container">
@@ -22,9 +24,30 @@ export class AppHome {
                 <a href="/activity" class="stat__link">View recent deliveries</a>
               </div>
             </div>
+            <div class="dashboard-bg"></div>
           </section>
-        </div>
-      </section>
+          <section class="home-secondary">
+            <div class="container">
+              <p>Pizza to the Polls is a non-partisan, nonprofit initiative founded in 2016 with a simple mission: deliver food to crowded polling locations.</p>
+              <a href="/donate" class="button secondary">Donate</a>
+              <div class="cards">
+                <div class="card">
+                  <h3>Food trucks</h3>
+                  <img src="/images/truck.jpg" alt="Food truck" />
+                  <p>For the 2020 election season, we’re launching a food truck program in 25 cities around the country for early voting and election day. </p>
+                  <a href="/trucks">Learn more</a>
+                </div>
+                <div class="card">
+                  <h3>On-demand delivery</h3>
+                  <img src="/images/pics/photo_5.jpg" alt="Pizza at a polling location" />
+                  <p>Our signature program is back and we need you to help by reporting crowded polling places and then sticking around to make sure food gets delivered safely.</p>
+                  <a href="/how-it-works">Learn more</a>
+                </div>
+              </div>
+            </div>
+          </section>
+        </section>
+      </div>
     );
   }
 }
