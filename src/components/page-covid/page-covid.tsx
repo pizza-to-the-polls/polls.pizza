bf928694b6fa894616e59e9958bf461cf26d95be
Noah Manger <noah.manger@gmail.com>
2020-10-14 11:32:40 -0700

    Update page-covid.tsx

--- src/components/page-covid/page-covid.tsx
+++ src/components/page-covid/page-covid.tsx
@@ -11,16 +11,54 @@ export class PageCovid {
           <div class="container">
             <h1>COVID Safety</h1>
             <p>
-              Pizza to the Polls values the health and safety of our communities, and will be working to mitigate risk of disease transmission. We encourage everyone visiting their
-              polling location in person to take extra precautions this year:
+              COVID-19 is a serious illness. Pizza to the Polls values the health and safety of our communities, and will be working to mitigate risk of disease transmission.
+              Everyone visiting their polling location in person should take{" "}
+              <a href="https://www.cdc.gov/coronavirus/2019-ncov/prevent-getting-sick/prevention.html" target="_blank">
+                extra precautions
+              </a>{" "}
+              this year:
             </p>
             <ul>
-              <li>Wear a mask.</li>
-              <li>Practice social distancing and stay at least 6 feet apart from others.</li>
-              <li>Wash hands frequently or use hand sanitizer that contains at least 60% alcohol.</li>
-              <li>Stay home when sick or after recent close contact with a person with COVID-19.</li>
-              <li>Make sure to comply with local guidance and COVID protocols put in place by your city or election officials.</li>
+              <li>
+                <a href="https://www.cdc.gov/coronavirus/2019-ncov/prevent-getting-sick/diy-cloth-face-coverings.html" target="_blank">
+                  Wear a mask
+                </a>
+                .
+              </li>
+              <li>
+                Practice{" "}
+                <a href="https://www.cdc.gov/coronavirus/2019-ncov/prevent-getting-sick/social-distancing.html" target="_blank">
+                  social distancing
+                </a>{" "}
+                and stay at least 6 feet apart from others.
+              </li>
+              <li>
+                <a href="https://www.cdc.gov/handwashing/when-how-handwashing.html" target="_blank">
+                  Wash hands
+                </a>{" "}
+                frequently or use hand sanitizer that contains at least 60% alcohol.
+              </li>
+              <li>
+                Be aware of the{" "}
+                <a href="https://www.cdc.gov/coronavirus/2019-ncov/symptoms-testing/symptoms.html" target="_blank">
+                  signs and symptoms
+                </a>
+                .
+              </li>
+              <li>Stay home if you are sick, experiencing any symptoms, or after recent close contact with a person with COVID-19.</li>
+              <li>Make sure to comply with local guidance and COVID-19 protocols put in place by your city or election officials.</li>
             </ul>
+            <p>
+              Our <a href="/trucks">food truck</a> program will be staffed by professionals who’ve been trained in food safety and policies to help reduce the spread of COVID-19,
+              but please keep in mind that it isn’t possible to eliminate the risk of exposure.
+            </p>
+            <p>
+              For <a href="/on-demand">on-demand</a> pizza deliveries, we will be supporting local pizzerias with our pizza delivery partners at Slice. We share{" "}
+              <a href="https://www.cdc.gov/coronavirus/2019-ncov/community/organizations/business-employers/bars-restaurants.html" target="_blank">
+                safety recommendations
+              </a>{" "}
+              with delivery drivers and restaurants, but it’s up to each person to consider their own personal risks.
+            </p>
           </div>
         </section>
       </Host>
