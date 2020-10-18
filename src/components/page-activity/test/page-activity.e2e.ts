27e26670de656b0dafe1d2645168512389af9a2b
Scott Duncombe <scott.duncombe@zapier.com>
2020-10-17 21:00:45 -0700

    Skip fetch tests

--- src/components/page-activity/test/page-activity.e2e.ts
+++ src/components/page-activity/test/page-activity.e2e.ts
@@ -1,11 +1,11 @@
 import { newE2EPage } from "@stencil/core/testing";
 
-describe("page-home", () => {
+describe("page-activity", () => {
   it.skip("renders", async () => {
     const page = await newE2EPage();
-    await page.setContent("<page-home></page-home>");
+    await page.setContent("<page-activity></page-activity>");
 
-    const element = await page.find("page-home");
+    const element = await page.find("page-activity");
     expect(element).toHaveClass("hydrated");
   });
 });
