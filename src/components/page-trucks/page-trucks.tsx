12de877cfd3a8cbb1fa16cd90915f03f26d58694
Noah Manger <noah.manger@gmail.com>
2020-10-18 18:26:04 -0700

    Prettier

--- src/components/page-trucks/page-trucks.tsx
+++ src/components/page-trucks/page-trucks.tsx
@@ -27,14 +27,14 @@ export class PageTrucks {
             <p>
               For the 2020 election season, Pizza to the Polls is launching a food truck program in partnership with Uber Eats and restaurant partners like Milk Bar, Shake Shack,
               and more. We’ll be sending out food trucks in 25 cities, delivering treats to polling locations with long lines throughout early voting and Election Day.
-              </p>
+            </p>
           </div>
           <div class="about">
             <div class="container">
               <p>
                 Since the COVID-19 pandemic began in March, we’ve seen in-person voting locations struggle. Poll worker shortages are reducing the number of polling places — and
-                social distancing measures are limiting the number of people who can vote at one time. As a result, lines are getting longer. By launching food trucks in cities with
-                a history of long lines, we plan to safely provide free, individually wrapped snacks and beverages to everyone.
+                social distancing measures are limiting the number of people who can vote at one time. As a result, lines are getting longer. By launching food trucks in cities
+                with a history of long lines, we plan to safely provide free, individually wrapped snacks and beverages to everyone.
               </p>
               <p>
                 Our food trucks will be staffed with trained professionals who will be wearing masks and gloves and will be equipped with hand sanitizer, soap, disinfectant, and
@@ -42,9 +42,7 @@ export class PageTrucks {
               </p>
             </div>
           </div>
-          <div class="hashtag">
-            #democracyisdelicious
-          </div>
+          <div class="hashtag">#democracyisdelicious</div>
           <div class="instructions">
             <div class="container">
               If you spot a long line in one of the cities below, report it <a href="/">here</a> and we'll send along a truck!
@@ -53,35 +51,85 @@ export class PageTrucks {
           <div class="container schedule">
             <h2>Locations and schedule</h2>
             <ul>
-              <li><strong>Phoenix, AZ</strong> Oct. 24, Oct. 29, Oc. 30, Nov. 3</li>
-              <li><strong>Los Angeles, CA</strong> Oct. 24, Oct. 29 - Nov. 3</li>
-              <li><strong>Washington, DC</strong> Oct. 30 - Nov. 3</li>
-              <li><strong>Miami, FL</strong> Oct. 24, Oct. 29 - Nov. 1, Nov. 3</li>
-              <li><strong>Tampa, FL</strong> Oct. 24, Oct. 29 - Nov. 1, Nov. 3</li>
-              <li><strong>Orlando, FL</strong> Oct. 24, Oct. 29 - Nov. 1, Nov. 3</li>
-              <li><strong>Gainesville, FL</strong> Oct. 24, Oct. 29 - Oct. 31, Nov. 3</li>
-              <li><strong>Atlanta, GA</strong> Oct. 24, Oct. 29, Oc. 30, Nov. 3</li>
-              <li><strong>Louisville, KY</strong> Oct. 24, Nov. 3</li>
-              <li><strong>Detroit, MI</strong> Oct. 24, Oct. 29 - Oct. 31, Nov.2, Nov. 3</li>
-              <li><strong>Ann Arbor, MI</strong> Oct. 24, Oct. 29 - Oct. 31, Nov.2, Nov. 3</li>
-              <li><strong>Minneapolis, MN</strong> Oct. 24, Oct. 29 - Nov. 3</li>
-              <li><strong>Charlotte, NC</strong> Oct. 24, Oct. 29 - Oct. 31, Nov. 3</li>
-              <li><strong>Raleigh, NC</strong> Oct. 24, Oct. 29 - Oct. 31, Nov. 3</li>
-              <li><strong>Greensboro, NC</strong> Oct. 24, Oct. 29 - Oct. 31, Nov. 3</li>
-              <li><strong>Las Vegas, NV</strong> Oct. 24, Oct. 29, Oct. 30, Nov. 3</li>
-              <li><strong>Reno, NV</strong> Oct. 24, Oct. 29, Oct. 30, Nov. 3</li>
-              <li><strong>New York, NY</strong> Oct. 24, Oct. 29 - Nov. 1, Nov. 3</li>
-              <li><strong>Philadelphia, PA</strong> Oct. 24, Oct. 29 - Nov. 3</li>
-              <li><strong>Pittsburgh, PA</strong> Oct. 24, Oct. 29 - Nov. 3</li>
-              <li><strong>Charleston, SC</strong> Oct. 24, Oct. 29 - Oct. 31, Nov. 3</li>
-              <li><strong>Nashville, TN</strong> Oct. 24, Oct. 29, Nov. 3</li>
-              <li><strong>Houston, TX</strong> Oct. 24, Oct. 29, Oct. 30, Nov. 3</li>
-              <li><strong>Austin, TX</strong> Oct. 24, Oct. 29, Oct. 30, Nov. 3</li>
-              <li><strong>Milwaukee, WI</strong> Oct. 24, Oct. 29 - Nov. 1, Nov. 3</li>
+              <li>
+                <strong>Phoenix, AZ</strong> Oct. 24, Oct. 29, Oc. 30, Nov. 3
+              </li>
+              <li>
+                <strong>Los Angeles, CA</strong> Oct. 24, Oct. 29 - Nov. 3
+              </li>
+              <li>
+                <strong>Washington, DC</strong> Oct. 30 - Nov. 3
+              </li>
+              <li>
+                <strong>Miami, FL</strong> Oct. 24, Oct. 29 - Nov. 1, Nov. 3
+              </li>
+              <li>
+                <strong>Tampa, FL</strong> Oct. 24, Oct. 29 - Nov. 1, Nov. 3
+              </li>
+              <li>
+                <strong>Orlando, FL</strong> Oct. 24, Oct. 29 - Nov. 1, Nov. 3
+              </li>
+              <li>
+                <strong>Gainesville, FL</strong> Oct. 24, Oct. 29 - Oct. 31, Nov. 3
+              </li>
+              <li>
+                <strong>Atlanta, GA</strong> Oct. 24, Oct. 29, Oc. 30, Nov. 3
+              </li>
+              <li>
+                <strong>Louisville, KY</strong> Oct. 24, Nov. 3
+              </li>
+              <li>
+                <strong>Detroit, MI</strong> Oct. 24, Oct. 29 - Oct. 31, Nov.2, Nov. 3
+              </li>
+              <li>
+                <strong>Ann Arbor, MI</strong> Oct. 24, Oct. 29 - Oct. 31, Nov.2, Nov. 3
+              </li>
+              <li>
+                <strong>Minneapolis, MN</strong> Oct. 24, Oct. 29 - Nov. 3
+              </li>
+              <li>
+                <strong>Charlotte, NC</strong> Oct. 24, Oct. 29 - Oct. 31, Nov. 3
+              </li>
+              <li>
+                <strong>Raleigh, NC</strong> Oct. 24, Oct. 29 - Oct. 31, Nov. 3
+              </li>
+              <li>
+                <strong>Greensboro, NC</strong> Oct. 24, Oct. 29 - Oct. 31, Nov. 3
+              </li>
+              <li>
+                <strong>Las Vegas, NV</strong> Oct. 24, Oct. 29, Oct. 30, Nov. 3
+              </li>
+              <li>
+                <strong>Reno, NV</strong> Oct. 24, Oct. 29, Oct. 30, Nov. 3
+              </li>
+              <li>
+                <strong>New York, NY</strong> Oct. 24, Oct. 29 - Nov. 1, Nov. 3
+              </li>
+              <li>
+                <strong>Philadelphia, PA</strong> Oct. 24, Oct. 29 - Nov. 3
+              </li>
+              <li>
+                <strong>Pittsburgh, PA</strong> Oct. 24, Oct. 29 - Nov. 3
+              </li>
+              <li>
+                <strong>Charleston, SC</strong> Oct. 24, Oct. 29 - Oct. 31, Nov. 3
+              </li>
+              <li>
+                <strong>Nashville, TN</strong> Oct. 24, Oct. 29, Nov. 3
+              </li>
+              <li>
+                <strong>Houston, TX</strong> Oct. 24, Oct. 29, Oct. 30, Nov. 3
+              </li>
+              <li>
+                <strong>Austin, TX</strong> Oct. 24, Oct. 29, Oct. 30, Nov. 3
+              </li>
+              <li>
+                <strong>Milwaukee, WI</strong> Oct. 24, Oct. 29 - Nov. 1, Nov. 3
+              </li>
             </ul>
           </div>
         </section>
-      </Host >
+      </Host>
     );
   }
 }
