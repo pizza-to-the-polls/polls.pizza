fdd508bc8258f6b395b3ee12cdfe2c7f4e9c2725
M. Griffie <mgriffie@nexus.engineering>
2020-10-26 23:29:33 -0700

    updated to stencil 2.0

--- stencil.config.ts
+++ stencil.config.ts
@@ -2,7 +2,7 @@ import replace from "@rollup/plugin-replace";
 import { Config } from "@stencil/core";
 import { sass } from "@stencil/sass";
 
-require("dotenv").config();
+require( "dotenv" ).config();
 
 // https://stenciljs.com/docs/config
 
@@ -10,29 +10,27 @@ export const config: Config = {
   globalStyle: "styles/main.scss",
   // globalScript: 'src/global/app.ts',
   taskQueue: "async",
+  plugins: [
+    replace( {
+      exclude: "node_modules/**",
+      values: {
+        "process.env.STRIPE_PUBLIC_KEY": process.env.STRIPE_PUBLIC_KEY ? `"${process.env.STRIPE_PUBLIC_KEY}"` : "process.env.STRIPE_PUBLIC_KEY",
+        "process.env.PIZZA_BASE_DOMAIN": process.env.PIZZA_BASE_DOMAIN ? `"${process.env.PIZZA_BASE_DOMAIN}"` : `"z"`,
+        "process.env.DONATION_FORM": process.env.DONATION_FORM ? `"${process.env.DONATION_FORM}"` : `"https://docs.google.com/forms/d/e/form-id/formResponse"`,
+      },
+    } ),
+    sass( {
+      injectGlobalPaths: ["styles/include/vars.scss", "styles/include/mixins.scss"],
+    } ),
+  ],
   outputTargets: [
     {
       type: "www",
-      serviceWorker: null,
       baseUrl: "https://polls.pizza/",
       dir: "dist/www",
       prerenderConfig: "./prerender.config.ts",
       copy: [{ src: "../public", dest: "." }],
     },
-  ],
-  buildEs5: "prod",
-  plugins: [
-    replace({
-      exclude: "node_modules/**",
-      values: {
-        "process.env.STRIPE_PUBLIC_KEY": process.env.STRIPE_PUBLIC_KEY ? `"${process.env.STRIPE_PUBLIC_KEY}"` : "process.env.STRIPE_PUBLIC_KEY",
-        "process.env.PIZZA_BASE_DOMAIN": process.env.PIZZA_BASE_DOMAIN ? `"${process.env.PIZZA_BASE_DOMAIN}"` : `"https://base.polls.pizza"`,
-        "process.env.DONATION_FORM": process.env.DONATION_FORM ? `"${process.env.DONATION_FORM}"` : `"https://docs.google.com/forms/d/e/form-id/formResponse"`,
-      },
-    }),
-    sass({
-      injectGlobalPaths: ["styles/include/vars.scss", "styles/include/mixins.scss"],
-    }),
   ],
   extras: {
     cssVarsShim: true,
