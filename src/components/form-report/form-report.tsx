4d9af188522a9770916f62f90932823e428ea414
Adam DeHaven <adam@dehaven.org>
2020-10-28 19:35:15 -0400

    Scroll to form

--- src/components/form-report/form-report.tsx
+++ src/components/form-report/form-report.tsx
@@ -1,6 +1,6 @@
 import { Build, Component, h, Host, State } from "@stencil/core";
 
-import { baseFetch, scrollPageToTop } from "../../lib/base";
+import { baseFetch } from "../../lib/base";
 
 // Shared with pizzabase
 const URL_REGEX = /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})).?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
@@ -158,7 +158,10 @@ export class FormReport {
       if (reportType) {
         reportType.checked = true;
       }
-      scrollPageToTop();
+      // Scroll to top
+      document.getElementById("form-report-component")?.scrollIntoView({
+        behavior: "smooth",
+      });
       this.showLocationInput = true;
     };
 
@@ -383,7 +386,9 @@ export class FormReport {
         // Disable submit
         this.isDisabled = true;
         // Scroll to top
-        scrollPageToTop();
+        document.getElementById("form-report-component")?.scrollIntoView({
+          behavior: "smooth",
+        });
         return false;
       }
 
@@ -467,13 +472,15 @@ export class FormReport {
         // Enable submit
         this.isDisabled = false;
         // Scroll to top
-        scrollPageToTop();
+        document.getElementById("form-report-component")?.scrollIntoView({
+          behavior: "smooth",
+        });
       }
     };
 
     return (
       <Host>
-        <div>
+        <div id="form-report-component">
           <form id="form-report" onChange={() => (this.isDisabled = false)} onInput={() => (this.isDisabled = false)} hidden={this.showConfirmation}>
             <div id="form-step-1" hidden={!this.showLocationInput}>
               {/* Intro content */}
