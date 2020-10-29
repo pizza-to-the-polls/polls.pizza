8b5b009894a498f84cae411fcffd2d31ec1084df
M. Griffie <mgriffie@nexus.engineering>
2020-10-29 05:58:16 -0700

    added marker option to ui-geo-map

--- src/components/ui-geo-map/ui-geo-map.tsx
+++ src/components/ui-geo-map/ui-geo-map.tsx
@@ -13,22 +13,31 @@ export class UiGeoMap {
   @Element() private el!: HTMLElement;
 
   private map?: google.maps.Map;
+  private marker?: google.maps.Marker;
 
   public componentDidLoad() {
     const m = this.el.shadowRoot!.querySelector("#map");
     this.map = new google.maps.Map(m as HTMLElement, {
       center: { lat: 38.8899, lng: -77.0091 },
-      zoom: 12,
+      zoom: 16,
       fullscreenControl: false,
       mapTypeControl: false,
       streetViewControl: false,
+      scrollwheel: false,
+    });
+    this.marker = new google.maps.Marker({
+      // icon: ,
+      map: this.map,
+      visible: false,
     });
   }
 
   @Method()
-  public setCenter(newCenter: google.maps.LatLngLiteral) {
+  public setCenter(newCenter: google.maps.LatLngLiteral, showMarker: boolean = false) {
     if (this.map) {
       this.map.setCenter(newCenter);
+      this.marker?.setPosition(newCenter);
+      this.marker?.setVisible(showMarker);
     }
     return DONE;
   }
