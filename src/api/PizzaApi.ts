887501176005e8d56350ccf77bd25cecb38dc48b
M. Griffie <mgriffie@nexus.engineering>
2020-10-29 05:56:21 -0700

    parsing Dates in API responses

--- src/api/PizzaApi.ts
+++ src/api/PizzaApi.ts
@@ -5,21 +5,29 @@ import { LocationId, LocationStatus, OrderDetails, OrderId, OrderQueryResults, P
 const BASE_URL = process.env.PIZZA_BASE_DOMAIN;
 
 // TODO: This should not need to be exported, remove once all commands are on `PizzaApi`
-export const baseFetch = async (path: string, options: { [key: string]: string } = {}): Promise<any> => {
+export const baseFetch = async ( path: string, options: { [key: string]: string } = {} ): Promise<any> => {
   const headers = options.headers || {};
-  const resp = await fetch(`${BASE_URL}${path}`, {
+  const resp = await fetch( `${BASE_URL}${path}`, {
     mode: "cors",
     ...options,
     headers: { "Content-Type": "application/json", ...headers },
-  });
+  } );
 
-  const data = await resp.json();
+  const text = await resp.text();
+  const data = JSON.parse( text, reviver );
 
-  if (resp.status > 299) {
+  if( resp.status > 299 ) {
     throw data.errors;
   }
 
   return data;
+};
+
+const reviver: ( this: any, key: string, value: any ) => any = ( key, value ) => {
+  if( key === "createdAt" || key === "validatedAt" ) {
+    return new Date( Date.parse( value ) );
+  }
+  return value;
 };
 
 /**
@@ -31,23 +39,23 @@ class PizzaApi {
    */
   public async getDonations(): Promise<number> {
     const { raised } = await getTotals();
-    return parseInt(raised.replace(",", ""), 10);
+    return parseInt( raised.replace( ",", "" ), 10 );
   }
 
-  public async getLocationStatus(normalizedAddress: string | LocationId): Promise<LocationStatus> {
-    return await baseFetch(`/locations/${encodeURIComponent(normalizedAddress)}`);
+  public async getLocationStatus( normalizedAddress: string | LocationId ): Promise<LocationStatus> {
+    return await baseFetch( `/locations/${encodeURIComponent( normalizedAddress )}` );
   }
 
-  public async getOrder(id: OrderId): Promise<OrderDetails> {
-    return await baseFetch(`/order/${id}`);
+  public async getOrder( id: OrderId ): Promise<OrderDetails> {
+    return await baseFetch( `/order/${id}` );
   }
 
-  public async getOrders(page: number = 0, limit: number = 100): Promise<OrderQueryResults> {
-    return await baseFetch(`/orders?page=${page}&limit=${limit}`);
+  public async getOrders( page: number = 0, limit: number = 100 ): Promise<OrderQueryResults> {
+    return await baseFetch( `/orders?page=${page}&limit=${limit}` );
   }
 
   public async getTotals(): Promise<PizzaTotals> {
-    return await baseFetch(`/totals/2020`);
+    return await baseFetch( `/totals/2020` );
   }
 }
 
