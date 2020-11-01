f2b4ff1bd3b3f37aa3cae76de4e4b9719573e3a2
M. Griffie <mgriffie@nexus.engineering>
2020-11-01 14:18:05 -0800

    added toQueryString helper

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,2 +1,3 @@
 export { default as debounce } from "./debounce";
 export { default as scrollPageToTop } from "./scrollPageToTop";
+export { default as toQueryString } from "./toQueryString";
---
+++ src/util/toQueryString.ts
@@ -0,0 +1,26 @@
+/**
+ * Convert `obj` into a URL-encoded querystring of key/value pairs
+ */
+function toQueryString(
+  obj: any,
+  includeQuestionmarkPrefix = false,
+): string {
+  if( obj == null ) {
+    return "";
+  }
+  const query = Object
+    .keys( obj )
+    .filter( k => obj[k] !== undefined )
+    .map( k => {
+      const val = obj[k];
+      const encodedKey = encodeURIComponent( k );
+      if( val instanceof Array ) {
+        return val.map( x => encodedKey + "=" + encodeURIComponent( x ) ).join( "&" );
+      } else {
+        return encodedKey + "=" + encodeURIComponent( val );
+      }
+    } )
+    .join( "&" );
+  return ( includeQuestionmarkPrefix && query !== "" ? "?" : "" ) + query;
+}
+export default toQueryString;
